99584eb870d3ecf05b849b317dcac749056749c0
ccaiml <abhishekgup@virtusa.com>
2019-01-10 19:08:48 +0000

    Code changes

---
+++ projects/home-credit-default-risk/lib/ConfigLib.py
@@ -0,0 +1,7 @@
+configDict={
+    'mlFlowUri' : '/home/ubuntu/applications/remoteDisk/platformData/mlruns',
+    'dataPath' : '/home/ubuntu/applications/remoteDisk/platformData/home-credit-default-risk/data/',
+    'modelPath' : '/home/ubuntu/applications/remoteDisk/platformData/home-credit-default-risk/models/',
+    'sharedLibPath' : '/home/abhishek/data-science-platform/shared'
+    
+}
